fd9673227f7eaa2acf0c5099f260c57f8bc61049
lukaszuznanski <lukaszuznanski94@gmail.com>
2016-07-12 00:18:36 +0200

    deleting books

--- src/MacoBundle/Controller/BookController.php
+++ src/MacoBundle/Controller/BookController.php
@@ -4,10 +4,13 @@ namespace MacoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use MacoBundle\Entity\Books;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class BookController extends Controller
 {
-    public function indexAction($slug)
+    public function indexAction($slug, Request $request)
     {
         $book = $this->getDoctrine()
             ->getRepository('MacoBundle:Books')
@@ -18,6 +21,25 @@ class BookController extends Controller
                 'Nie ma książki o takim tytule.'
             );
         }
-        return $this->render('MacoBundle:Book:index.html.twig', array('book' => $book));
+
+        $bookDelete = new Books();
+
+        $form = $this->createFormBuilder($bookDelete)
+            ->add('save', SubmitType::class, array('label' => 'Usuń'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $repo = $em->getRepository('MacoBundle:Books');
+            $bookToDelete = $repo->findOneBy(array('title' => $slug));
+            $em->remove($bookToDelete);
+            $em->flush();
+
+            return $this->redirectToRoute('maco_homepage');
+        }
+        return $this->render('MacoBundle:Book:index.html.twig', array('book' => $book, 'form' => $form->createView()));
     }
 }
